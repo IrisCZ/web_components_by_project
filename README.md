a79918db278f7e7d7f3a30b2acc10f2550a9a7ad
Iris <iris.carballo@jobandtalent.com>
2019-06-09 11:51:16 +0200

    web components - the beginning

---
+++ README.md
@@ -0,0 +1,20 @@
+# Web Components the begining
+
+## Info
+
+This project is a beginning to start using Web components. It shows a way to create a couple of components that are connected between them, not only to generate a component but also to see how you can pass data from one to another. In the end we'll have four buttons that, when you click them, they'll show you some movie lines.
+
+The project is deployed [here](https://iriscz.github.io/web_components_the_beginning/) And it's very close related with this [article](https://medium.com/p/7577e5cf2262) in which I explain the proccess developing it.
+
+## Authors
+
+Iris Carballo
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Thanks a lot for the tips to [Jesús Jimenez](https://twitter.com/jjballano). 
+* Thanks to Mariam Soliman from unsplash.com to share her art.
---
+++ data.js
@@ -0,0 +1,26 @@
+const data = {
+    'title': 'Movies', 
+    'options': {
+        'Scarface': [
+        'Say hello to my little friend!',
+        '...the eyes, chico, never lie!...',
+        'I\'m Tony Montana! You fuck with me, you fuckin\' with the best!'
+        ],
+        'The Jazz Singer':[
+            'Wait a minute, wait a minute. You ain\'t heard nothin\' yet!',
+            'We in the show business have our religion too <br> on every day, the show must go on!'
+        ],
+        'Fight Club':[
+            'It’s only after we’ve lost everything that we’re free to do anything.',
+            'You wanna make an omelet, you gotta break some eggs.',
+            'The things you own end up owning you.',
+            'The first rule of Fight Club is: You do not talk about Fight Club. <br> The second rule of Fight Club is: You do not talk about Fight Club.'
+        ],
+        'The Truman Show': [
+            'Good morning, and in case I don\'t see ya: <br> Good afternoon, good evening, and good night!',
+            'You can\'t get any further away, before you start coming back.',
+            'That\'s a whole lot of world for one man.',
+            'Somebody help me, I\'m being spontaneous!'
+        ]
+    }
+};
